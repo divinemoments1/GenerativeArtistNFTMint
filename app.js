d01eb8ae48d7806a1f6e42a5e6682ff22a799219
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2023-08-13 17:07:43 -0500

    cleaned up html, added fixed navbar, added anchors, added hero image

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
 
 app.use(express.static("public"));
 
