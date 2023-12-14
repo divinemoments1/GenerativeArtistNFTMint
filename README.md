b9012c0ac4c9e983ee9b15eac8bcbb975d826564
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2023-12-13 19:32:39 -0600

    replaced old stuff with updated files

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# GenerativeArtistNFTMint
+# MACT6340Project
 
-A personal mint for a generative artist to build, deploy, and sell NFTs of their projects on EVM blockchains.
+Web3 project for MACT 6340
+
+This is the course project example / guide repo.
+
+To see the project in a different state, use the "tags" tab on github. This will allow you to download a zip file with the code depicted the repo in its state at varying milestones while under development.
--- app.js
+++ app.js
@@ -1,12 +1,66 @@
 import express from "express";
+import * as utils from "./utils/utils.js";
+import * as db from "./utils/database.js";
+import dotenv from "dotenv";
+dotenv.config();
+let projects = [];
+
 const app = express();
 const port = 3000;
-
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
-
+app.set("view engine", "ejs");
+app.use(express.json());
 app.use(express.static("public"));
+
+app.get("/", async (req, res, next) => {
+  await db
+    .connect()
+    .then(async () => {
+      // query the databse for project records
+      projects = await db.getAllProjects();
+      console.log(projects);
+      let featuredRand = Math.floor(Math.random() * projects.length);
+      res.render("index.ejs", { featuredProject: projects[featuredRand] });
+    })
+    .catch(next);
+});
+
+app.get("/projects", (req, res) => {
+  res.render("projects.ejs", { projectArray: projects });
+});
+
+app.get("/project/:id", (req, res) => {
+  let id = req.params.id;
+  if (id > data.length) {
+    throw new Error("No project with that ID");
+  }
+  res.render("project.ejs", { project: projects[id - 1], which: id });
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact.ejs");
+});
+
+app.post("/mail", async (req, res) => {
+  await utils
+    .sendMessage(req.body.sub, req.body.txt)
+    .then(() => {
+      res.send({ result: "success" });
+    })
+    .catch(() => {
+      res.send({ result: "failure" });
+    });
+});
+
+app.use(async (err, req, res, next) => {
+  console.log(err);
+  let msg;
+  msg = err.message;
+  if (msg != "No project with that ID") {
+    msg =
+      "There was an internal error. Apologies. We are working on cleaning up the mess.";
+  }
+  res.render("error.ejs", { msg: msg });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
